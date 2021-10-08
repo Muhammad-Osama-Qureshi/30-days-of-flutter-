a02e5da68d09ebb199af6bc1b8258c64261b894f
Gulsher Khan Zai <78407903+GulsherKhan659@users.noreply.github.com>
2021-10-09 00:07:10 +0500

    Update main.dart

--- day6/lib/main.dart
+++ day6/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day6/widgets/themes.dart';
+import 'package:day6/widgets/themes.dart'; //import
 import 'package:flutter/material.dart';
 import 'package:day6/pages/login_page.dart';
 import 'package:day6/utils/routes.dart';
